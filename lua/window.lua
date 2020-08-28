9dabc97654032843f225a7d0351a46863b558b55
ishan <ishan365.ia@gmail.com>
2020-08-28 13:52:09 +0530

    initial working of my window rules

--- lua/window.lua
+++ lua/window.lua
@@ -1,21 +1,37 @@
 local api = vim.api
 
-local buf,win
+local M = {}
 
-local function create_win()
-  start_win = api.nvim_get_current_win()
+function M.create_win(position, per_height, per_width, command)
 
-  api.nvim_command('vnew')
-  win = api.nvim_get_current_win()
-  buf = api.nvim_get_current_buf()
+	local t_height = vim.fn.nvim_win_get_height(0)
+	local t_width  = vim.fn.nvim_win_get_width(0)
 
-  vim.fn.nvim_win_set_width(win,25)
+	api.nvim_command(position)
 
-  vim.bo[buf].buftype = 'nofile'
-  vim.bo[buf].swapfile = false
-  vim.bo[buf].bufhidden = 'wipe'
-  api.nvim_win_set_option(win, 'wrap', false)
-  api.nvim_win_set_option(win, 'cursorline', true)
+	-- Create the window and set the dimensions
+	local win = api.nvim_get_current_win()
+
+	if per_height ~= nil then
+		local height = per_height * t_height
+		height = math.ceil(height)
+		vim.fn.nvim_win_set_height(win,height)
+	end
+
+	if per_width ~= nil then
+		local width = per_width * t_width
+		width = math.ceil(width)
+		vim.fn.nvim_win_set_width(win,width)
+	end
+
+	-- Chech if you want to list the buffer
+	local buf = api.nvim_create_buf(true,false)
+	api.nvim_set_current_buf(buf)
+
+	if command ~= nil then
+		api.nvim_command(command)
+	end
+
 end
 
-create_win()
+return M
