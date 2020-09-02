22dfe2c170374ee9047e5ffd6f5d974cc2ce8073
ishan <ishan365.ia@gmail.com>
2020-09-02 14:44:05 +0530

    remove the configuration that doesn't need to be in lua

--- init.vim
+++ init.vim
@@ -2,11 +2,6 @@
 set termguicolors
 luafile ~/.config/nvim/init.lua
 
-" ----- Plugin Settings -----
-" Settings for my plugins
-lua require('lsp')
-lua require('Ultisnips')
-
 command! -nargs=0 -bar Helptags
     \  for p in glob('~/.vim/pack/bundle/opt/*', 1, 1)
     \|     exe 'packadd ' . fnamemodify(p, ':t')
--- lua/Ultisnips.lua
+++
@@ -1,8 +0,0 @@
-local api = vim.api
--- Ultisnips{{{1
--- Trigger configuration. Do not use <tab> if you use https://github.com/Valloric/YouCompleteMe.
-api.nvim_set_var('UltiSnipsExpandTrigger', '<tab>') -- Use <Tab> to trigger autocompletion
-api.nvim_set_var('UltiSnipsJumpForwardTrigger', '<tab>')
-api.nvim_set_var('UltiSnipsJumpBackwardTrigger', '<s-tab>')
-
-
