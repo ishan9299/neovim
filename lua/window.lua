fcf680e62816918e31faa71cbd19dc56dc95dd87
ishan <ishan365.ia@gmail.com>
2020-09-05 07:44:29 +0530

    initial commit for the buffer window

--- lua/window.lua
+++ lua/window.lua
@@ -1,36 +1,118 @@
+local vim = vim
 local api = vim.api
 
-local M = {}
+local buf,winId
 
-function M.create_win(options)
-
-	local t_height = vim.fn.nvim_win_get_height(0)
-	local t_width  = vim.fn.nvim_win_get_width(0)
-	api.nvim_command(options["position"])
-
-	-- Create the window and set the dimensions
-	local win = api.nvim_get_current_win()
-
-	if options["height"] ~= nil then
-		local height = options["height"] * t_height
-		height = math.ceil(height)
-		vim.fn.nvim_win_set_height(win,height)
-	end
-
-	if options["width"] ~= nil then
-		local width = options["width"] * t_width
-		width = math.ceil(width)
-		vim.fn.nvim_win_set_width(win,width)
-	end
-
-	-- Chech if you want to list the buffer
-	local buf = api.nvim_create_buf(true,false)
-	api.nvim_set_current_buf(buf)
-
-	if options["command"] ~= nil then
-		api.nvim_command(options["command"])
-	end
-
+local function open()
+	local buf_num = string.byte[2]
+	api.nvim_command('b ' .. buf_num)
 end
 
-return M
+local function close()
+end
+
+local function open_close()
+	open()
+	close()
+end
+
+local function createWindow()
+	local stats = api.nvim_list_uis()[1]
+	local width = stats.width
+	local height = stats.height
+
+	buf = api.nvim_create_buf(false,false)
+	vim.bo[buf].buftype = 'nofile'
+	vim.bo[buf].swapfile = false
+	vim.bo[buf].bufhidden = 'wipe'
+
+	local options = {
+		relative = 'editor',
+		height = height-4,
+		width = width-4,
+		row = 2,
+		col = 2
+	}
+	winId = api.nvim_open_win(buf, true, options)
+	vim.wo[winId].number = false
+	vim.wo[winId].cursorline = true
+	vim.wo[winId].wrap = false
+	vim.wo[winId].relativenumber = false
+end
+
+local function list()
+	vim.bo[buf].modifiable = true
+	local bufferList = api.nvim_list_bufs()
+
+	-- Remove the Scratch buffers from the bufferList
+	for _ , number in ipairs(bufferList) do
+		if api.nvim_buf_is_loaded(number) then
+			local swapfile = api.nvim_buf_get_option(number,'swapfile')
+			local listed = api.nvim_buf_get_option(number,'buflisted')
+			local modifiable = api.nvim_buf_get_option(number,'modifiable')
+			if not (swapfile and listed and modifiable) then
+				print("buffer removed " , number )
+				table.remove(bufferList,_)
+			end
+		end
+	end
+
+	local names = {}
+	for index,number in pairs(bufferList) do
+		-- print(api.nvim_buf_line_count(number))
+		if api.nvim_buf_is_loaded(number) then
+			local buffer = api.nvim_buf_get_name(number)
+			buffer = '[' .. number .. '] ' .. buffer
+			table.insert(names,index,buffer)
+		end
+	end
+
+	api.nvim_buf_set_lines(buf , 0 , -1 , false , names)
+	vim.bo[buf].modifiable = false
+end
+
+local function switch()
+	local buffer = vim.api.nvim_get_current_line()
+	print(buffer)
+end
+
+local function set_mappings()
+	local mappings = {
+		q = 'close()',
+		['<cr>'] = 'open_and_close()',
+		-- v = 'split("v")',
+		-- s = 'split("")',
+		-- p = 'preview()',
+		-- t = 'open_in_tab()'
+	}
+
+	for k,v in pairs(mappings) do
+		local opts = {nowait = true, noremap = true , silent = true}
+		vim.api.nvim_buf_set_keymap(buf, 'n', k, ':lua require"nvim-oldfile".'..v..'<cr>', opts)
+	end
+end
+
+local function buffers()
+	if winId and vim.api.nvim_win_is_valid(winId) then
+		api.nvim_set_current_win(winId)
+	else
+		createWindow()
+	end
+	list()
+end
+
+buffers()
+
+-- TODO write an function to switch buffers
+
+
+return {
+	createWindow = createWindow,
+	list = list,
+	open = open,
+	close = close,
+	open_close = open_close,
+	switch = switch,
+	set_mappings = set_mappings,
+	buffers = buffers
+}
