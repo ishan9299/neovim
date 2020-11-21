a184546a5d40cd4780567fdce6b54274f2cd9931
ishan <ishan365.ia@gmail.com>
2020-11-21 15:34:29 +0530

    LazyGit don't use neovim-remote.

--- lua/core/plugins/lazygit.lua
+++ lua/core/plugins/lazygit.lua
@@ -2,4 +2,4 @@ local vim = vim
 vim.cmd('packadd! lazygit')
 vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
 vim.g.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
-vim.g.lazygit_use_neovim_remote = 1 -- nvr integration
+vim.g.lazygit_use_neovim_remote = 0 -- nvr integration
