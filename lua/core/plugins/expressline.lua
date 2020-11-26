488dc6ddae5f5ea1b45939ab398c19ecdd75c82a
ishan <ishan365.ia@gmail.com>
2020-11-26 06:00:52 +0530

    Update the submodules

--- lua/core/plugins/expressline.lua
+++ lua/core/plugins/expressline.lua
@@ -6,29 +6,31 @@ vim.cmd('packadd! expressline')
 local generator = function()
   local el_segments = {}
 
-  -- Option 2, just a function that returns a string.
-  local extensions = require('el.extensions')
-  table.insert(el_segments, extensions.mode) -- mode returns the current mode.
-
-  -- expresss_line provides a helpful wrapper for these.
-  -- You can check out el.builtin
   local builtin = require('el.builtin')
-  table.insert(el_segments, ' ')
-  table.insert(el_segments, builtin.tail)
-  table.insert(el_segments, ' ')
-  table.insert(el_segments, builtin.modified)
-  table.insert(el_segments, '%=')
-  table.insert(el_segments, builtin.filetype)
-  table.insert(el_segments, ' ')
+  local extensions = require('el.extensions')
+  local subscribe = require('el.subscribe')
 
-  -- Option 4, you can return a coroutine.
-  --  In lua, you can cooperatively multi-thread.
-  --  You can use `coroutine.yield()` to yield execution to another coroutine.
-  --
-  -- For example, in luvjob.nvim, there is `co_wait` which is a coroutine version of waiting for a job to complete. So you can start multiple jobs at once and wait for them to all be done.
-  table.insert(el_segments, extensions.git_changes)
+  table.insert(el_segments, extensions.mode)
+   table.insert(el_segments,
+    subscribe.buf_autocmd(
+      "el_file_icon",
+      "BufRead",
+      function(_, buffer)
+        return extensions.file_icon(_, buffer)
+      end
+    ))
+    table.insert(el_segments, builtin.shortened_file)
 
-  return el_segments
+   table.insert(el_segments,
+    subscribe.buf_autocmd(
+      "el_git_branch",
+      "BufEnter",
+      function(window, buffer)
+        return extensions.git_branch(window, buffer)
+      end
+    ))
+
+    return el_segments
 end
 
 -- And then when you're all done, just call
--- lua/core/plugins/treesitter.lua
+++ lua/core/plugins/treesitter.lua
@@ -1,8 +1,8 @@
 vim.cmd('packadd! treesitter')
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "all",     -- one of "all", "language", or a list of languages
+  ensure_installed = "maintained",     -- one of "all", "language", or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
-    -- disable = { "c", "rust" },  -- list of language that will be disabled
+    use_languagetree = false,
   },
 }
