a7213bce75b0c0e559ec9b210a4c65bbd613ec5c
ishan <ishan365.ia@gmail.com>
2020-12-15 16:54:42 +0530

    changes

--- init.lua
+++ init.lua
@@ -3,27 +3,34 @@ local wo = vim.wo
 local g = vim.g
 local bo = vim.bo
 
-g.mapleader = ' '
-
-o.termguicolors = true
-
 vim.cmd('filetype plugin indent on') -- Filetype flygin on
 vim.cmd('syntax enable') -- enable syntax highlighting
+vim.cmd('colorscheme modus-operandi')
 
-o.scrolloff = 5 -- Scrolling lines starts 5 lines above the last one
 
+-- ++------ WINDOW OPTIONS ------++
 -- Foldmethod
 wo.foldmethod='expr'
 wo.foldlevel=99
 wo.foldexpr='nvim_treesitter#foldexpr()'
+-- Relative line numbers to get normal line number set relative line number as false
+wo.relativenumber = true
+wo.number = true
+-- Enables some characters that show up when using tab and trailing spaces
+wo.list=true
+-- Cursor line
+wo.cursorline = true
 
+
+-- ++------ GLOBAL OPTIONS ------++
 -- Tabstop
-o.tabstop = 2
-bo.tabstop = 2
-
+o.ts=4
+o.sts=4
+o.shiftwidth=4 -- spaces for autoindent
+o.expandtab = true -- Turn of tabs
 -- Set the path to find the file in a project
+o.termguicolors = true
 o.path = o.path .. '**'
-
 
 local ignore = o.wildignore
 ignore = ignore .. '*.o,*.obj,*.bin,*.dll,*.exe,'
@@ -31,46 +38,48 @@ ignore = ignore .. '*/.git/*,*/.svn/*,*/__pycache__/*,*/build/**,'
 ignore = ignore .. '*.pyc,'
 ignore = ignore .. '*.DS_Store,'
 ignore = ignore .. '*.aux,*.bbl,*.blg,*.brf,*.fls,*.fdb_latexmk,*.synctex.gz,*.pdf,'
-
 o.wildignore = ignore -- Ignore certain files and folders when globbing
 
 o.hidden = true -- Switch buffers painlessly
-
 o.lazyredraw = true -- Macros don't show any animation
-
--- Do not load netrw by default since I do not use it, see
--- https://github.com/bling/dotvim/issues/4
-g.loaded_netrwPlugin = 1
-
-g.loaded_2html_plugin = 1 -- Do not load tohtml.vim
-
 -- The eob removes ~ at the end of buffer
 o.fillchars = 'diff:∙,fold:·,vert:┃,eob: '
-
 -- Show tabs and spaces
-wo.list=true
 o.listchars='nbsp:⦸,extends:»,precedes:«,trail:•,tab:▸ '
-
--- Cursor line
-wo.cursorline = true
-
 -- Show line break
 o.showbreak = '↳ '
-
 o.showcmd = false
-
 -- These setting are needed for better completion behaviour and some annoyances
-o.completeopt = 'menuone,noinsert,noselect'
-o.shortmess = vim.o.shortmess .. 'cWIa'
-
 -- c = completion messages
 -- W = don't echo "[w]"/"[written]" when writing
 -- I = no splash screen
 -- a = use abbreviations in messages eg. `[RO]` instead of `[readonly]`
+o.completeopt = 'menuone,noinsert,noselect'
+o.shortmess = vim.o.shortmess .. 'cWIa'
+-- statusline
+-- 2 = always show it
+-- 1 = show only when there is a split
+-- 0 = never show it
+o.laststatus = 2
+-- Scrolling lines starts 5 lines above the last one
+o.scrolloff = 5
+-- Font used by neovim gui
+o.guifont = 'Iosevka Nerd Font Mono:h17'
 
-o.ts=4 -- tab width
-o.sts=4
-o.shiftwidth=4 -- spaces for autoindent
-o.expandtab = true -- Turn of tabs
 
+-- ++------ GLOBAL VARIABLES ------++
+-- Set the Leader key
+g.mapleader = ' '
+-- Disable the built-in plugins
+g.loaded_netrwPlugin = 1
+g.loaded_2html_plugin = 1 -- Do not load tohtml.vim
+g.loaded_vimball = 1
+g.loaded_justify = 1
+g.loaded_cfilter = 1
+g.loaded_shellmenu = 1
+g.loaded_termdebug = 1
+
+
+
+-- Lua files
 require('core')
