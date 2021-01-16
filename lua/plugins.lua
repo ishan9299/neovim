c52e516a64a73d46f233c527c6054e70953c30a7
ishan <ishan365.ia@gmail.com>
2021-01-17 04:23:48 +0530

    fix packer config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,3 @@
-local g = vim.g
-local remap = vim.api.nvim_set_keymap
 local utils = require('utils')
 
 local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
@@ -34,14 +32,14 @@ return require('packer').startup(function()
     use {
         'wbthomason/packer.nvim',
         opt = true,
-        config = function()
-            local compile_packer = {
-                packer = {
-                    { "BufWritePost", "plugins.lua", "PackerCompile" }
-                }
-            }
-            utils.nvim_create_augroups(compile_packer)
-        end
+        -- config = function()
+        --     local compile_packer = {
+        --         packer = {
+        --             { "BufWritePost", "plugins.lua", "PackerCompile" }
+        --         }
+        --     }
+        --     utils.nvim_create_augroups(compile_packer)
+        -- end,
     }
 
     -- Pretty colors
@@ -77,6 +75,8 @@ return require('packer').startup(function()
     use {
         'kyazdani42/nvim-tree.lua',
         config = function()
+            local g = vim.g
+            local remap = vim.api.nvim_set_keymap
             g.lua_tree_side = 'left' -- left by default
             g.lua_tree_width = 40 -- 30 by default
             g.lua_tree_auto_open            = 0 -- 0 by default, opens the tree when typing `vim $DIR` or `vim`
@@ -149,6 +149,7 @@ return require('packer').startup(function()
     use {
         'nvim-lua/completion-nvim',
         config = function()
+            local g = vim.g
             g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
             g.completion_auto_change_source = 1
 
@@ -170,12 +171,12 @@ return require('packer').startup(function()
                 }
             }
 
-            local completion_autocmds = {
-                completion = {
-                    {"BufEnter", "*", "lua require'completion'.on_attach()"}
-                }
-            }
-            utils.nvim_create_augroups(completion_autocmds)
+        --     local completion_autocmds = {
+        --         completion = {
+        --             {"BufEnter", "*", "lua require'completion'.on_attach()"}
+        --         }
+        --     }
+        --     utils.nvim_create_augroups(completion_autocmds)
 
         end,
     }
@@ -197,6 +198,7 @@ return require('packer').startup(function()
     use {
         'mbbill/undotree',
         config = function()
+            local remap = vim.api.nvim_set_keymap
             remap('n', '<F5>', ':UndotreeToggle<cr>', { silent = true, noremap = true })
         end,
     }
@@ -213,7 +215,7 @@ return require('packer').startup(function()
     use {
         'ziglang/zig.vim',
         config = function()
-            g.zig_fmt_autosave = 1
+            vim.g.zig_fmt_autosave = 1
         end,
     }
 
@@ -221,6 +223,7 @@ return require('packer').startup(function()
         'nvim-telescope/telescope.nvim',
         requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
         config = function()
+            local remap = vim.api.nvim_set_keymap
             local opts = { silent = true, noremap = true }
             remap("n" , "tff" , "<cmd>lua require('telescope.builtin').find_files()<cr>" , opts)
             remap("n" , "tgf" , "<cmd>lua require('telescope.builtin').git_files()<cr>"  , opts)
@@ -234,9 +237,110 @@ return require('packer').startup(function()
         '~/.config/nvim/modus-theme-vim',
         requires = { {'tjdevries/colorbuddy.nvim'}, {'glepnir/galaxyline.nvim'} },
         config = function()
+            local g = vim.g
             g.modus_moody_enable = 1
             g.modus_faint_syntax = 1
             require('colorbuddy').colorscheme('modus-vivendi')
+        end,
+    }
+
+    use {
+        'neovim/nvim-lspconfig',
+        config = function()
+            local nvim_lsp = require('lspconfig')
+            local on_attach = function(client, bufnr)
+                local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+                local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+                buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+                -- Mappings.
+                local opts = { noremap=true, silent=true }
+                buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+                buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+                buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+                buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+                buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+                buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+                buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+                buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+                buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+                buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+                buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+                buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+                buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+                buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+                buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+
+                -- Set some keybinds conditional on server capabilities
+                if client.resolved_capabilities.document_formatting then
+                    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+                elseif client.resolved_capabilities.document_range_formatting then
+                    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+                end
+
+                -- Set autocommands conditional on server_capabilities
+                if client.resolved_capabilities.document_highlight then
+                    require('lspconfig').util.nvim_multiline_command [[
+                    :hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+                    :hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+                    :hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+                    augroup lsp_document_highlight
+                    autocmd!
+                    autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+                    autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+                    augroup END
+                    ]]
+                end
+            end
+
+            -- Use a loop to conveniently both setup defined servers 
+            -- and map buffer local keybindings when the language server attaches
+            local servers = { "pyright", "rust_analyzer", "tsserver" }
+            for _, lsp in ipairs(servers) do
+                nvim_lsp[lsp].setup { on_attach = on_attach }
+            end
+
+            local system_name
+            if vim.fn.has("mac") == 1 then
+                system_name = "macOS"
+            elseif vim.fn.has("unix") == 1 then
+                system_name = "Linux"
+            elseif vim.fn.has('win32') == 1 then
+                system_name = "Windows"
+            else
+                print("Unsupported system for sumneko")
+            end
+
+            -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
+            local sumneko_root_path = vim.fn.stdpath('cache')..'/lspconfig/sumneko_lua/lua-language-server'
+            local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+
+            require'lspconfig'.sumneko_lua.setup {
+                cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+                settings = {
+                    Lua = {
+                        runtime = {
+                            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                            version = 'LuaJIT',
+                            -- Setup your lua path
+                            path = vim.split(package.path, ';'),
+                        },
+                        diagnostics = {
+                            -- Get the language server to recognize the `vim` global
+                            globals = {'vim'},
+                        },
+                        workspace = {
+                            -- Make the server aware of Neovim runtime files
+                            library = {
+                                [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                                [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                            },
+                        },
+                    },
+                },
+            }
+
         end,
     }
 
