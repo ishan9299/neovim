b3750fdddfbd3d9572939938ba594d7d7995b6c7
ishan <ishan365.ia@gmail.com>
2021-02-25 11:15:32 +0530

    remove the long comment

--- init.lua
+++ init.lua
@@ -240,31 +240,6 @@ return require('packer').startup(function()
                 }
             }
 
-            --[[
-            " You can edit keybindings be defining this variable
-            " You don't have to define all keys.
-            " NOTE: the 'edit' key will wrap/unwrap a folder and open a file
-            let g:lua_tree_bindings = {
-            \ 'edit':            ['<CR>', 'o'],
-            \ 'edit_vsplit':     '<C-v>',
-            \ 'edit_split':      '<C-x>',
-            \ 'edit_tab':        '<C-t>',
-            \ 'toggle_ignored':  'I',
-            \ 'toggle_dotfiles': 'H',
-            \ 'refresh':         'R',
-            \ 'preview':         '<Tab>',
-            \ 'cd':              '<C-]>',
-            \ 'create':          'a',
-            \ 'remove':          'd',
-            \ 'rename':          'r',
-            \ 'cut':             'x',
-            \ 'copy':            'c',
-            \ 'paste':           'p',
-            \ 'prev_git_item':   '[c',
-            \ 'next_git_item':   ']c',
-            \ }
-            --]]
-
             remap('n', '<C-e>', ':NvimTreeToggle<cr>', { silent = true, noremap = true })
         end,
     }
