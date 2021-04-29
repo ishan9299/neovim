fd201285177f640af31ee9d4e82e9bc309c30e95
ishan <ishan365.ia@gmail.com>
2021-04-29 17:01:42 +0530

    add some plugins

--- init.lua
+++ init.lua
@@ -59,7 +59,80 @@ wo.list = true
 ---------- Plugins ----------
 cmd('packadd! tabular')
 cmd('packadd! vim-commentary')
-cmd('packadd! vim-dirvish')
+-- cmd('packadd! vim-dirvish')
+cmd('packadd! plenary')
+cmd('packadd! popup')
+cmd('packadd! nvim-web-devicons')
+cmd('packadd! lir')
+cmd('packadd! lir-bookmarks')
+cmd('packadd! lir-mmv')
+
+local actions = require'lir.actions'
+local mark_actions = require 'lir.mark.actions'
+local clipboard_actions = require'lir.clipboard.actions'
+
+require'lir'.setup {
+  show_hidden_files = false,
+  devicons_enable = true,
+  mappings = {
+    ['l']     = actions.edit,
+    ['<C-s>'] = actions.split,
+    ['<C-v>'] = actions.vsplit,
+    ['<C-t>'] = actions.tabedit,
+
+    ['h']     = actions.up,
+    ['q']     = actions.quit,
+
+    ['K']     = actions.mkdir,
+    ['N']     = actions.newfile,
+    ['R']     = actions.rename,
+    ['@']     = actions.cd,
+    ['Y']     = actions.yank_path,
+    ['.']     = actions.toggle_show_hidden,
+
+    ['J'] = function()
+      mark_actions.toggle_mark()
+      vim.cmd('normal! j')
+    end,
+    ['C'] = clipboard_actions.copy,
+    ['X'] = clipboard_actions.cut,
+    ['P'] = clipboard_actions.paste,
+  },
+  float = {
+    size_percentage = 0.5,
+    winblend = 15,
+    border = true,
+    borderchars = {"╔" , "═" , "╗" , "║" , "╝" , "═" , "╚", "║"},
+    shadow = true,
+  },
+  hide_cursor = true,
+}
+
+-- custom folder icon
+require'nvim-web-devicons'.setup({
+  override = {
+    lir_folder_icon = {
+      icon = "",
+      color = "#7ebae4",
+      name = "LirFolderNode"
+    },
+  }
+})
+
+-- use visual mode
+function _G.LirSettings()
+  vim.api.nvim_buf_set_keymap(0, 'x', 'J', ':<C-u>lua require"lir.mark.actions".toggle_mark("v")<CR>', {noremap = true, silent = true})
+
+  -- echo cwd
+  vim.api.nvim_echo({{vim.fn.expand('%:p'), 'Normal'}}, false, {})
+end
+
+vim.cmd [[augroup lir-settings]]
+vim.cmd [[  autocmd!]]
+vim.cmd [[  autocmd Filetype lir :lua LirSettings()]]
+vim.cmd [[augroup END]]
+map('n', '-', [[:lua require'lir.float'.toggle()<cr>]], normal_mode_silent)
+
 cmd('packadd! vim-repeat')
 cmd('packadd! vim-surround')
 cmd('packadd! vim-startuptime')
@@ -157,8 +230,6 @@ cmd('packadd! vim-go')
 cmd('packadd! BetterLua')
 ----------- Telescope -----------
 cmd('packadd! nvim-telescope')
-cmd('packadd! plenary')
-cmd('packadd! popup')
 map('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>",
     normal_mode_silent)
 map('n', '<leader>lg', "<cmd>lua require('telescope.builtin').live_grep()<cr>",
