190d2a972b69259fe0f81e8db0aa4531825f7f5c
ishan <ishan365.ia@gmail.com>
2021-04-29 17:29:06 +0530

    changes

--- init.lua
+++ init.lua
@@ -67,64 +67,81 @@ cmd('packadd! lir')
 cmd('packadd! lir-bookmarks')
 cmd('packadd! lir-mmv')
 
-local actions = require'lir.actions'
+local actions = require 'lir.actions'
 local mark_actions = require 'lir.mark.actions'
-local clipboard_actions = require'lir.clipboard.actions'
+local clipboard_actions = require 'lir.clipboard.actions'
+local mmv_actions = require 'lir.mmv.actions'
+local bookmark_actions = require 'lir.bookmark.actions'
 
 require'lir'.setup {
-  show_hidden_files = false,
-  devicons_enable = true,
-  mappings = {
-    ['l']     = actions.edit,
-    ['<C-s>'] = actions.split,
-    ['<C-v>'] = actions.vsplit,
-    ['<C-t>'] = actions.tabedit,
+    show_hidden_files = false,
+    devicons_enable = true,
+    mappings = {
+        ['l'] = actions.edit,
+        ['<C-s>'] = actions.split,
+        ['<C-v>'] = actions.vsplit,
+        ['<C-t>'] = actions.tabedit,
 
-    ['h']     = actions.up,
-    ['q']     = actions.quit,
+        ['h'] = actions.up,
+        ['q'] = actions.quit,
+        ['<C-[>'] = actions.quit,
 
-    ['K']     = actions.mkdir,
-    ['N']     = actions.newfile,
-    ['R']     = actions.rename,
-    ['@']     = actions.cd,
-    ['Y']     = actions.yank_path,
-    ['.']     = actions.toggle_show_hidden,
+        ['K'] = actions.mkdir,
+        ['N'] = actions.newfile,
+        ['R'] = actions.rename,
+        ['@'] = actions.cd,
+        ['Y'] = actions.yank_path,
+        ['.'] = actions.toggle_show_hidden,
 
-    ['J'] = function()
-      mark_actions.toggle_mark()
-      vim.cmd('normal! j')
-    end,
-    ['C'] = clipboard_actions.copy,
-    ['X'] = clipboard_actions.cut,
-    ['P'] = clipboard_actions.paste,
-  },
-  float = {
-    size_percentage = 0.5,
-    winblend = 15,
-    border = true,
-    borderchars = {"╔" , "═" , "╗" , "║" , "╝" , "═" , "╚", "║"},
-    shadow = true,
-  },
-  hide_cursor = true,
+        ['J'] = function()
+            mark_actions.toggle_mark()
+            vim.cmd('normal! j')
+        end,
+        ['C'] = clipboard_actions.copy,
+        ['X'] = clipboard_actions.cut,
+        ['P'] = clipboard_actions.paste,
+
+        ['M'] = mmv_actions.mmv,
+
+        ['B'] = bookmark_actions.list,
+        ['ba'] = bookmark_actions.add
+    },
+    hide_cursor = true
+}
+
+local b_actions = require 'lir.bookmark.actions'
+require'lir.bookmark'.setup {
+    bookmark_path = '~/.lir_bookmark',
+    mappings = {
+        ['l'] = b_actions.edit,
+        ['<C-s>'] = b_actions.split,
+        ['<C-v>'] = b_actions.vsplit,
+        ['<C-t>'] = b_actions.tabedit,
+        ['<C-e>'] = b_actions.open_lir,
+        ['B'] = b_actions.open_lir,
+        ['q'] = b_actions.open_lir
+    }
 }
 
 -- custom folder icon
 require'nvim-web-devicons'.setup({
-  override = {
-    lir_folder_icon = {
-      icon = "",
-      color = "#7ebae4",
-      name = "LirFolderNode"
-    },
-  }
+    override = {
+        lir_folder_icon = {
+            icon = "",
+            color = "#7ebae4",
+            name = "LirFolderNode"
+        }
+    }
 })
 
 -- use visual mode
 function _G.LirSettings()
-  vim.api.nvim_buf_set_keymap(0, 'x', 'J', ':<C-u>lua require"lir.mark.actions".toggle_mark("v")<CR>', {noremap = true, silent = true})
+    vim.api.nvim_buf_set_keymap(0, 'x', 'J',
+                                ':<C-u>lua require"lir.mark.actions".toggle_mark("v")<CR>',
+                                {noremap = true, silent = true})
 
-  -- echo cwd
-  vim.api.nvim_echo({{vim.fn.expand('%:p'), 'Normal'}}, false, {})
+    -- echo cwd
+    vim.api.nvim_echo({{vim.fn.expand('%:p'), 'Normal'}}, false, {})
 end
 
 vim.cmd [[augroup lir-settings]]
