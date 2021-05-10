85d3064ad27e2bf7bc41df14a5eecb7cbaae362e
ishan <ishan365.ia@gmail.com>
2021-05-10 23:56:17 +0530

    add expressline config

--- lua/ishan/plugins/init.lua
+++ lua/ishan/plugins/init.lua
@@ -1,6 +1,6 @@
 require('ishan.plugins.lir')
 require('ishan.plugins.devicons')
--- require('ishan.plugins.expressline')
+require('ishan.plugins.expressline')
 require('ishan.plugins.fterm')
 require('ishan.plugins.lspconfig')
 require('ishan.plugins.telescope')
